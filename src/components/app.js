77bcea48d9ce8f78660d3489903b0c7ec4ddfd7c
Ersan <smokecode@gmail.com>
2016-05-22 12:27:05 -0400

    add middleware redux promise

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Component } from 'react';
-import HouseList from './house_detail';
+import HouseList from '../containers/house_list';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         GoT API
-        <div>
-          <button className='btn'>Get</button>
           <HouseList />
-        </div>
       </div>
     )
   }
